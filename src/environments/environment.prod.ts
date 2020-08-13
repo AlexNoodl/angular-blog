9f78579884316cf797b09acd611407e3fec4c9b2
Alexander <alex.noodl@gmail.com>
2020-08-13 09:36:20 +0300

    Finish app

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
-export const environment = {
-  production: true
+import {Environment} from "./interface";
+
+export const environment: Environment = {
+  production: true,
+  apiKey: 'AIzaSyBaGX90DLevKRRg6C3VftnUabNFHMPgv7w',
+  fbDbUrl: 'https://angular-blog-dfa77.firebaseio.com'
 };
